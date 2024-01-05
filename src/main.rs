bfcacadc0e8dddd19153a8e163a08ae144c00c95
Thaza_Kun <61819672+Thaza-Kun@users.noreply.github.com>
2024-01-06 01:55:58 +0800

    :hammer: REFACTOR: Functions and constructors

--- src/main.rs
+++ src/main.rs
@@ -9,89 +9,128 @@ const DEFAULT_PRECISION: usize = 5;
 
 struct Measure {
     value: f64,
-    uncertainty: Uncertainty<f64, usize>,
+    uncertainty: Uncertainty,
 }
 
 impl Measure {
-    fn with_no_err(value: f64) -> Measure {
+    fn scalar(value: f64) -> Measure {
         Measure {
             value,
-            uncertainty: Uncertainty::Certain,
+            uncertainty: Uncertainty::null(),
         }
     }
-    fn with_rel_err(value: f64, err: f64) -> Measure {
+
+    fn with_rel_err(self, err: f64) -> Measure {
         Measure {
-            value,
-            uncertainty: Uncertainty::Relative(err.abs(), DEFAULT_PRECISION),
+            value: self.value,
+            uncertainty: Uncertainty::symmetric_rel(err),
         }
     }
-    fn with_abs_err(value: f64, err: f64) -> Measure {
+
+    fn with_abs_err(self, err: f64) -> Measure {
         Measure {
-            value,
-            uncertainty: Uncertainty::Absolute(err.abs(), DEFAULT_PRECISION),
+            value: self.value,
+            uncertainty: Uncertainty::symmetric_abs(err),
         }
     }
+
     fn with_precision(&self, precision: usize) -> Self {
         Self {
             value: self.value,
             uncertainty: self.uncertainty.with_precision(precision),
         }
     }
+
+    fn resolve_high_low_limits(&self) -> (f64, f64) {
+        let unc = self.uncertainty.to_absolute(self.value);
+        (self.value + unc.high, self.value - unc.low)
+    }
+}
+#[derive(Copy, Clone, Debug)]
+enum UncertaintyVariant {
+    Absolute,
+    Relative,
 }
 
-enum Uncertainty<T, P> {
-    Absolute(T, P),
-    Relative(T, P),
-    Certain,
+#[derive(Copy, Clone, Debug)]
+struct Uncertainty {
+    low: f64,
+    high: f64,
+    precision: usize,
+    variant: UncertaintyVariant,
 }
 
-impl Uncertainty<f64, usize> {
-    fn unwrap(&self) -> f64 {
-        match self {
-            Uncertainty::Absolute(v, _) => *v,
-            Uncertainty::Relative(v, _) => *v,
-            Uncertainty::Certain => 0.,
+#[allow(dead_code)]
+impl Uncertainty {
+    fn null() -> Self {
+        Self {
+            low: 0.,
+            high: 0.,
+            precision: DEFAULT_PRECISION,
+            variant: UncertaintyVariant::Absolute,
         }
     }
-    fn with_precision(&self, precision: usize) -> Uncertainty<f64, usize> {
-        match self {
-            Uncertainty::Absolute(v, _) => Uncertainty::Absolute(*v, precision),
-            Uncertainty::Relative(v, _) => Uncertainty::Relative(*v, precision),
-            Uncertainty::Certain => Uncertainty::Absolute(0., precision),
+
+    fn symmetric_abs(value: f64) -> Self {
+        Self {
+            low: value.abs(),
+            high: value.abs(),
+            variant: UncertaintyVariant::Absolute,
+            ..Self::null()
         }
     }
-    fn precision(&self) -> usize {
-        match self {
-            Uncertainty::Absolute(_, p) => *p,
-            Uncertainty::Relative(_, p) => *p,
-            Uncertainty::Certain => DEFAULT_PRECISION,
+    fn symmetric_rel(value: f64) -> Self {
+        Self {
+            low: value.abs(),
+            high: value.abs(),
+            variant: UncertaintyVariant::Relative,
+            ..Self::null()
         }
     }
-    fn to_absolute(&self, value: f64) -> Uncertainty<f64, usize> {
-        match self {
-            Uncertainty::Absolute(v, p) => Uncertainty::Absolute(*v, *p),
-            Uncertainty::Relative(v, p) => {
-                Uncertainty::Absolute((v / 100.) * value.abs() as f64, *p)
+
+    fn non_symmetric(low: f64, high: f64) -> Self {
+        Self {
+            low,
+            high,
+            ..Self::null()
+        }
+    }
+
+    fn with_precision(self, precision: usize) -> Self {
+        Self { precision, ..self }
+    }
+
+    fn to_absolute(self, value: f64) -> Uncertainty {
+        if let UncertaintyVariant::Relative = self.variant {
+            Self {
+                low: (self.low / 100.) * value.abs(),
+                high: (self.high / 100.) * value.abs(),
+                variant: UncertaintyVariant::Absolute,
+                ..self
             }
-            Uncertainty::Certain => Uncertainty::Absolute(0., DEFAULT_PRECISION),
+        } else {
+            self
         }
     }
-    fn to_relative(&self, value: f64) -> Uncertainty<f64, usize> {
-        match self {
-            Uncertainty::Absolute(v, p) => {
-                Uncertainty::Relative((v / value.abs() as f64) * 100., *p)
+    fn to_relative(self, value: f64) -> Uncertainty {
+        if let UncertaintyVariant::Absolute = self.variant {
+            Self {
+                low: (self.low - value.abs()).abs() / value.abs() * 100.,
+                high: (self.high - value.abs()).abs() / value.abs() * 100.,
+                variant: UncertaintyVariant::Relative,
+                ..self
             }
-            Uncertainty::Relative(v, p) => Uncertainty::Relative(*v, *p),
-            Uncertainty::Certain => Uncertainty::Relative(0., DEFAULT_PRECISION),
+        } else {
+            self
         }
     }
 }
 
 fn main() {
-    let a = Measure::with_no_err(1.23).with_precision(1) + Measure::with_abs_err(2.0, 0.1);
-    let b = Measure::with_no_err(1.25) - Measure::with_rel_err(2.0, 50.).with_precision(3);
-    let c = Measure::with_no_err(1.28).with_precision(1) * Measure::with_abs_err(2.0, 0.1);
-    let d = Measure::with_no_err(12.) / Measure::with_abs_err(2.91, 0.1).with_precision(2);
+    let a = Measure::scalar(1.23).with_precision(1) + Measure::scalar(2.0).with_rel_err(50.);
+    let b = Measure::scalar(1.25) - Measure::scalar(2.0).with_rel_err(50.).with_precision(3);
+    let c = Measure::scalar(1.28).with_precision(1) * Measure::scalar(2.0).with_abs_err(0.1);
+    let d = Measure::scalar(12.) / Measure::scalar(2.91).with_abs_err(0.1).with_precision(2);
 
     println!("a \t= {}", &a);
     println!("b \t= {}", &b);
@@ -103,45 +142,52 @@ fn main() {
 
 impl Display for Measure {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let (high, low) = self.resolve_high_low_limits();
         write!(
             f,
-            "{:>pad$.precision$} ± {:>pad$.precision$} \t:= ({:>pad$.precision$}, {:>pad$.precision$})",
+            "{:>pad$.precision$} {:>pad$.precision$} \n\t:= ({:>pad$.precision$}, {:>pad$.precision$})",
             self.value,
             self.uncertainty,
-            self.value - self.uncertainty.to_absolute(self.value).unwrap(),
-            self.value + self.uncertainty.to_absolute(self.value).unwrap(),
-            precision = self.uncertainty.precision(),
+            low,
+            high,
+            precision = self.uncertainty.precision,
             pad = 0
         )
     }
 }
 
-impl Display for Uncertainty<f64, usize> {
+impl Display for Uncertainty {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            Uncertainty::Absolute(..) => {
-                write!(
-                    f,
-                    "{:.precision$}",
-                    self.unwrap(),
-                    precision = self.precision()
-                )
-            }
-            Uncertainty::Relative(..) => {
-                write!(
-                    f,
-                    "{:.precision$} %",
-                    self.unwrap(),
-                    precision = self.precision()
-                )
-            }
-            Uncertainty::Certain => write!(
+        if self.low != self.high {
+            write!(
                 f,
-                "{:.precision$}",
-                self.unwrap(),
-                precision = self.precision()
-            ),
+                "+{:.precision$}{symbol} / -{:.precision$}{symbol}",
+                self.high,
+                self.low,
+                precision = self.precision,
+                symbol = match self.variant {
+                    UncertaintyVariant::Absolute => "",
+                    UncertaintyVariant::Relative => "%",
+                }
+            )
+        } else {
+            write!(
+                f,
+                "±{:.precision$}{symbol}",
+                self.high,
+                precision = self.precision,
+                symbol = match self.variant {
+                    UncertaintyVariant::Absolute => "",
+                    UncertaintyVariant::Relative => "%",
+                }
+            )
         }
+    }
+}
+
+impl Default for Uncertainty {
+    fn default() -> Self {
+        Self::null()
     }
 }
 
@@ -151,45 +197,48 @@ impl Add for Measure {
     fn add(self, rhs: Self) -> Self::Output {
         Self {
             value: self.value + rhs.value,
-            uncertainty: Uncertainty::Absolute(
-                self.uncertainty.to_absolute(self.value).unwrap()
-                    + rhs.uncertainty.to_absolute(rhs.value).unwrap(),
-                self.uncertainty
-                    .precision()
-                    .min(rhs.uncertainty.precision()),
-            ),
+            uncertainty: self.uncertainty.to_absolute(self.value)
+                + rhs.uncertainty.to_absolute(rhs.value),
         }
     }
 }
+
 impl Sub for Measure {
     type Output = Self;
 
     fn sub(self, rhs: Self) -> Self::Output {
         Self {
             value: self.value - rhs.value,
-            uncertainty: Uncertainty::Absolute(
-                self.uncertainty.to_absolute(self.value).unwrap()
-                    + rhs.uncertainty.to_absolute(rhs.value).unwrap(),
-                self.uncertainty
-                    .precision()
-                    .min(rhs.uncertainty.precision()),
-            ),
+            uncertainty: self.uncertainty.to_absolute(self.value)
+                + rhs.uncertainty.to_absolute(rhs.value),
         }
     }
 }
+
+impl Add for Uncertainty {
+    type Output = Self;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        match (self.variant, rhs.variant) {
+            (UncertaintyVariant::Absolute, UncertaintyVariant::Absolute) => Self {
+                low: self.low + rhs.low,
+                high: self.high + rhs.high,
+                precision: self.precision.min(rhs.precision),
+                variant: UncertaintyVariant::Absolute,
+            },
+            (_, _) => panic!("Please convert both uncertainty to its absolute variants apply additive propagation, {:#?} + {:#?}", &self, &rhs),
+        }
+    }
+}
+
 impl Div for Measure {
     type Output = Self;
 
     fn div(self, rhs: Self) -> Self::Output {
         Self {
             value: self.value / rhs.value,
-            uncertainty: Uncertainty::Relative(
-                self.uncertainty.to_relative(self.value).unwrap()
-                    + rhs.uncertainty.to_relative(rhs.value).unwrap(),
-                self.uncertainty
-                    .precision()
-                    .min(rhs.uncertainty.precision()),
-            ),
+            uncertainty: self.uncertainty.to_relative(self.value)
+                * rhs.uncertainty.to_relative(rhs.value),
         }
     }
 }
@@ -199,13 +248,25 @@ impl Mul for Measure {
     fn mul(self, rhs: Self) -> Self::Output {
         Self {
             value: self.value * rhs.value,
-            uncertainty: Uncertainty::Relative(
-                self.uncertainty.to_relative(self.value).unwrap()
-                    + rhs.uncertainty.to_relative(rhs.value).unwrap(),
-                self.uncertainty
-                    .precision()
-                    .min(rhs.uncertainty.precision()),
-            ),
+            uncertainty: self.uncertainty.to_relative(self.value)
+                * rhs.uncertainty.to_relative(rhs.value),
+        }
+    }
+}
+
+impl Mul for Uncertainty {
+    type Output = Self;
+
+    fn mul(self, rhs: Self) -> Self::Output {
+        match (self.variant, rhs.variant) {
+            (UncertaintyVariant::Relative, UncertaintyVariant::Relative) => {
+                 Self {
+                low: self.low + rhs.low,
+                high: self.high + rhs.high,
+                precision: self.precision.max(rhs.precision),
+                variant: UncertaintyVariant::Relative,
+            }},
+            (_,_) => panic!("Please convert both uncertainty to its relative variant to apply multiplicative propagation")
         }
     }
 }
